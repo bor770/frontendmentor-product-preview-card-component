58acec29fec2976849d41ff89ace64dcf0d739de
Baruch Giverts <bor770@gmail.com>
2023-05-18 16:02:25 +0300

    in middle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'product-preview-card-component';
+
+  constructor(
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.iconRegistry.addSvgIcon(
+      `cart`,
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        `../assets/images/icon-cart.svg`
+      )
+    );
+  }
 }
